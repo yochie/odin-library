57a825b763200fd45309fcfc601469518b1d91bf
yoann <yoann.ochietti@gmail.com>
2024-11-04 14:11:55 -0500

    Enable read/unread button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myLibrary = [];
-const library = document.querySelector(".library");
+const libraryDOM = document.querySelector(".library");
 
 
 function Book(title, author, pages, read) {
@@ -9,7 +9,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-Book.prototype.toggleRead = function(){
+Book.prototype.toggleRead = function () {
     this.read = !this.read;
 }
 
@@ -18,40 +18,52 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-function displayBooks(){
+function displayBooks() {
+    let children = [];
     let index = 0;
-    for(let book of myLibrary){
+    for (let book of myLibrary) {
         let row = document.createElement("div");
         row.classList.add("book");
-        row.setAttribute("data-attribute", index);
 
         let header = document.createElement("h2");
         header.textContent = book.title;
         row.appendChild(header);
 
         let author = document.createElement("h3");
-        author.textContent = book.author;
+        author.textContent = `by: ${book.author}`;
         row.appendChild(author);
 
         let pages = document.createElement("p");
-        pages.textContent = book.pages;
+        pages.textContent = `Pages: ${book.pages}`;
         row.appendChild(pages);
 
         let read = document.createElement("p");
-        read.textContent = book.read ? "Yes" : "No";
+        read.textContent = `Read: ${book.read ? "Yes" : "No"}`;
         row.appendChild(read);
 
         let button = document.createElement("button");
         button.textContent = book.read ? "Mark unread" : "Mark read";
+        button.setAttribute("data-attribute", index);
         row.appendChild(button);
 
-        library.appendChild(row);
+        children.push(row);
         index++;
     }
+    libraryDOM.replaceChildren(...children);
 }
 
+libraryDOM.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "BUTTON")
+        return;
+
+    const id = event.target.getAttribute("data-attribute");
+    myLibrary[id].toggleRead();
+
+    displayBooks();
+});
+
 //test
-addBookToLibrary("the hobbit", "jr tolkien", 100);
+addBookToLibrary("the hobbit", "jr tolkien", 100, true);
 addBookToLibrary("the hobbit 2", "jr tolkien", 200);
 addBookToLibrary("lord of the rings", "jr tolkien", 500);
 console.log(myLibrary);
